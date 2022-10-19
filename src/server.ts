c4839da8adc11aec7acd12b7aa59effef49bfca0
mrgreque <gabrielgreque9910@gmail.com>
2022-10-19 02:24:54 -0300

    :bug: fix port listen

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { startMongoClient } from './provider/client';
 config();
 
 
-app.listen(5555, async () => {
+app.listen(process.env.PORT || 5555, async () => {
     await startMongoClient();
     console.log("Server is running!");
 });
