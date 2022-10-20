a65e51a49b0d21a3b63c60ef8dcd4f13281d4ddc
Kener Wendel <kenerwendel@gmail.com>
2022-10-20 15:26:44 -0300

    added cors dependency to server.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { router } from './routes/routes';
 import 'express-async-errors';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
